7f31a8a2d933678f90ac84a72968116e8aded55e
akiosu <87393490+Akiosuu@users.noreply.github.com>
2025-08-28 17:47:59 +0300

    Update gpu_hash_searcher.cpp

--- gpu_hash_searcher.cpp
+++ gpu_hash_searcher.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #include <thread>
 
-// CUDA kernel for parallel hash computation 
+// CUDA kernel for parallel hash computation
 __device__ const char d_chars[] = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 
 __device__ void cuda_sha1(const char* input, int len, unsigned char* hash) {
@@ -331,3 +331,4 @@ int main() {
     return 0;
 
 }
+
